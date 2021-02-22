2916a6bdb6630a621f1026ca4ff57e3919258a21
stars_inc <stars_inc@mail.ru>
2021-02-22 10:47:54 +0300

    feat: solve for ones

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+
+  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 
+  'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 
+  'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+
+  tens = ['', '', 'twenty ', 'thirty ', 'forty ', 
+  'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
+
+  if(number < 20) return ones[number]
 }
