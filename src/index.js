de0ceda162a36f3582e011744af1baf6b6cd2bb9
stars_inc <stars_inc@mail.ru>
2021-02-27 19:30:53 +0300

    feat: that was explosive my brain

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 module.exports = function toReadable(number) {
+  if (number < 1) return 'zero'
+  
 
-  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 
-  'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 
-  'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-
-  tens = ['', '', 'twenty ', 'thirty ', 'forty ', 
-  'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety ']
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 
+  'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 
+  'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const tens = ['', '', 'twenty', 'thirty', 
+  'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
   if(number < 20) return ones[number]
+
+  switch(number) {
+    case number >= 10 && number < 20: return teens[number - 10]
+    default: return `${tens[~~(number / 10)]} ${ones[number % 10]}`
+  }
+
 }
